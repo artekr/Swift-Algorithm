edc7887c02cc44fc7ee7dc8b8bb90ac6ab67d8e9
Heng Wang <kevinwang264@gmail.com>
2021-03-03 22:44:52 -0500

    added gitignore

--- data_structure/queue/Queue.swift
+++ data_structure/queue/Queue.swift
@@ -4,35 +4,35 @@
 //
 /*
  FIFO
- 
+
  A queue is a list where you can only insert new items at the back and remove items from the front.
- 
- 
+
+
  */
 
 public struct Queue<T> {
-  
-  fileprivate var array = [T]()
-  
+
+  private var array = [T]()
+
   public var isEmpty: Bool {
     return array.isEmpty
   }
-  
+
   public var count: Int {
     return array.count
   }
-  
+
   public var front: T? {
     return array.first
   }
-  
+
   // O(1)
   // Because an array in Swift always has some empty space at the end. Adding a new element is just copying memory from one place to another which is a constant-time operation.
   public mutating func enqueue(_ element: T) {
     array.append(element)
   }
-  
-  // O(n) 
+
+  // O(n)
   // Because it requires all remaining array elements to be shifted in memory.
   public mutating func dequeue() -> T? {
     if isEmpty {
@@ -48,18 +48,18 @@ public struct Queue<T> {
 // MARK: - A more efficient queue
 
 public struct MoreEfficientQueue<T> {
-  
-  fileprivate var array = [T?]()
-  fileprivate var head = 0        // The index in the array of the front-most object.
-  
+
+  private var array = [T?]()
+  private var head = 0        // The index in the array of the front-most object.
+
   public var isEmpty: Bool {
     return count == 0
   }
-  
+
   public var count: Int {
     return array.count - head
   }
-  
+
   public var front: T? {
     if isEmpty {
       return nil
@@ -67,20 +67,20 @@ public struct MoreEfficientQueue<T> {
       return array[head]
     }
   }
-  
+
   public mutating func enqueue(_ element: T) {
     array.append(element)
   }
-  
+
   // Whenever we dequeue an item, we do not shift the contents of the array to the front (slow) but mark the item's position in the array as empty (fast).
   public mutating func dequeue() -> T? {
     guard head < array.count, let element = array[head] else {
       return nil
     }
-    
+
     array[head] = nil
     head += 1
-    
+
     // To periodically trim down the array, we do the following:
     let percentage = Double(head) / Double(array.count)
     if array.count > 50 && percentage > 0.25 {
@@ -89,13 +89,4 @@ public struct MoreEfficientQueue<T> {
     }
     return element
   }
-  
 }
-
-
-
-
-
-
-
-
